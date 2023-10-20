11fcb7942d9c2a0c723da30937ceb439cc9bea81
30082866 <30082866@CLIENT>
2023-10-20 12:09:06 +0800

    Initial GUI design and basic classes added.

---
+++ Drone.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneServiceApplication
+{
+    internal class Drone
+    {
+        public string clientName = string.Empty;
+        public string droneModel = string.Empty;
+        public string serviceProblem = string.Empty;
+        public int serviceCost = 0;
+        public string serviceTag = string.Empty;
+
+        public Drone() { }
+
+        public Drone(string _clientName, string _droneModel, string _serviceProblem, int _serviceCost, string _serviceTag)
+        {
+            clientName = _clientName;
+            droneModel = _droneModel;
+            serviceProblem = _serviceProblem;
+            serviceCost = _serviceCost;
+            serviceTag = _serviceTag;
+        }
+
+        // Getters and Setters
+        public string GetClientName()
+        {
+            return clientName;
+        }
+        public void SetClientName(string _value)
+        {
+            clientName = _value;
+        }
+        public string GetDroneModel()
+        {
+            return droneModel;
+        }
+        public void SetDroneModel(string _value)
+        {
+            droneModel = _value;
+        }
+        public string GetServiceProblem()
+        {
+            return serviceProblem;
+        }
+        public void SetServiceProblem(string _value)
+        {
+            serviceProblem = _value;
+        }
+        public int GetServiceCost()
+        {
+            return serviceCost;
+        }
+        public void SetServiceCost(int _value)
+        {
+            serviceCost = _value;
+        }
+        public string GetServiceTag()
+        {
+            return serviceTag;
+        }
+        public void SetServiceTag(string _value)
+        {
+            serviceTag = _value;
+        }
+    }
+}
